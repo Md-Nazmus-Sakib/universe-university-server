b718f59f062c8d45583b01e6dbcf813bdad47c5a
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-09 20:13:05 +0600

    semester-registration-model

--- src/app/modules/SemesterRegistration/semesterRegistration.interface.ts
+++ src/app/modules/SemesterRegistration/semesterRegistration.interface.ts
@@ -4,6 +4,7 @@ export type TSemesterRegistration = {
   academicSemester: Types.ObjectId;
   status: "UPCOMING" | "ONGOING" | "ENDED";
   startDate: Date;
+  endDate: Date;
   minCredit: number;
   maxCredit: number;
 };
