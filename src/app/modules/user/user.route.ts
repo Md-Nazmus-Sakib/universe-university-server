f505b315371c759a0f34a3663b23f93fa4ba845e
Md-Nazmus-Sakib <nazmussakib0036@gmail.com>
2024-06-09 10:12:36 +0600

    create-user-as faculty

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,13 +2,27 @@ import express from "express";
 import { UserControllers } from "./user.controller";
 import validateRequest from "../../middlewares/validateRequest";
 import { createStudentValidationSchema } from "../student/student.validation";
+import { createFacultyValidationSchema } from "../Faculty/faculty.validation";
 
 const router = express.Router();
-
+//=============================================
+//Create Student Route
 router.post(
   "/create-student",
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent
 );
+
+//===============================================
+
+//===========================================
+//Create Faculty Route
+router.post(
+  "/create-faculty",
+
+  validateRequest(createFacultyValidationSchema),
+  UserControllers.createFaculty
+);
+//==========================================
 
 export const UserRoutes = router;
